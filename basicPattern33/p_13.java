b69aa34119ba5b1706ef3c8a41f17331055d61c8
Ishu6129 <165049137+Ishu6129@users.noreply.github.com>
2024-08-14 16:07:11 +0530

    p_13.java

--- basicPattern33/p_13.java
+++ basicPattern33/p_13.java
@@ -1,26 +1,22 @@
 package GLA.pattern;
-
+import java.util.*;
 public class p_13 {
     public static void main(String[] arg){
-        int a=5;
-        int i=0;
-        while (i<a){
-            int j=-1;
-            while (j<i){
+        Scanner sc=new Scanner(System.in);
+        int b=sc.nextInt();
+        int a=2*b-1;
+        int i=1;
+        while (i<a+1){
+            int j=0;
+            int f=i;
+            if (f>b){
+                f=a-i+1;
+            }
+            while (j<f){
                 System.out.print("* ");
                 j++;
             }
             i++;
-            System.out.println();
-        }
-        int k=0;
-        while (k<(a-1)){
-            int j=0;
-            while (j<(a-k-1)){
-                System.out.print("* ");
-                j++;
-            }
-            k++;
             System.out.println();
         }
     }
