5926cb5c7d54156caf3ad654b2d73076624a1d56
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2024-11-20 23:43:42 +0530

    Add files via upload

---
+++ HackerBlocks/Assignment_5/StringToggleCase.java
@@ -0,0 +1,18 @@
+package Assignment_5;
+
+import java.util.Scanner;
+
+public class StringToggleCase {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) >= 65 && s.charAt(i) <= 90){
+                System.out.print((char)(32+(int)s.charAt(i)));
+            }
+            else{
+                System.out.print((char)((int)s.charAt(i)-32));
+            }
+        }
+    }
+}
