65f1ed5e01a652af6920c588d2a60b633089ba9a
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-08 23:10:17 +0530

    Create ArrayList_of_levels _BinaryTree_LeetCode_102.java

---
+++ HackerBlocks/Assignment 8/ArrayList_of_levels _BinaryTree_LeetCode_102.java
@@ -0,0 +1,82 @@
+import java.util.*;
+public class Main {
+
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Main m = new Main();
+		BinaryTree bt1 = m.new BinaryTree();
+		System.out.println(bt1.levelArrayList());
+	}
+
+	private class BinaryTree {
+		private class Node {
+			int data;
+			Node left;
+			Node right;
+		}
+
+		private Node root;
+		private int size;
+
+		public BinaryTree() {
+			this.root = this.takeInput(null, false);
+		}
+
+		public Node takeInput(Node parent, boolean ilc) {
+
+			int cdata = scn.nextInt();
+			Node child = new Node();
+			child.data = cdata;
+			this.size++;
+
+			// left
+			boolean hlc = scn.nextBoolean();
+
+			if (hlc) {
+				child.left = this.takeInput(child, true);
+			}
+
+			// right
+			boolean hrc = scn.nextBoolean();
+
+			if (hrc) {
+				child.right = this.takeInput(child, false);
+			}
+
+			// return
+			return child;
+		}
+
+        public ArrayList<ArrayList<Integer>> levelArrayList() {
+            ArrayList<ArrayList<Integer>> ll = new ArrayList<>();
+            Queue<Node> q = new LinkedList<>();
+            
+            if (this.root != null) {
+                q.add(this.root);
+            }
+            
+            while (!q.isEmpty()) {
+                int lsize = q.size();
+                ArrayList<Integer> arr = new ArrayList<>();
+                
+                int count = 0;
+                while (count < lsize) {
+                    Node n = q.poll();
+                    arr.add(n.data);
+                    if (n.left != null) {
+                        q.add(n.left);
+                    }
+                    if (n.right != null) {
+                        q.add(n.right);
+                    }
+                    count++;
+                }   
+                ll.add(arr);
+            }
+            return ll;
+        }
+
+	}
+
+}
