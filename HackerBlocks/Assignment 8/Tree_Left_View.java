95883630d151115eae6c0a936b8372b02da0ed39
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-09 21:36:15 +0530

    Create Tree_Left_View.java

---
+++ HackerBlocks/Assignment 8/Tree_Left_View.java
@@ -0,0 +1,123 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String[] arr=br.readLine().split(" ");
+        BinaryTree bt=new BinaryTree(arr);
+        bt.leftView();
+        
+    }
+}
+
+class BinaryTree
+{
+    private class Node
+    {
+        int data;
+        Node left,right;
+        Node(int data)
+        {
+            this.data=data;
+        }
+    }
+
+    Node root;
+
+    BinaryTree(String[] arr)
+    {
+        Queue<Node> queue=new LinkedList<Node>();
+        construct(arr,0,queue);
+    }
+
+    public void leftView()
+    {
+        leftView(this.root,1);
+    }
+
+	static int mxd=0;
+    private void leftView(Node root,int cl) {
+		if(root.data==-1 || root==null){
+			return;
+        }
+        if(mxd<cl){
+            System.out.print(root.data+" ");
+            mxd=cl;
+        }
+        leftView(root.left,cl+1);
+        leftView(root.right,cl+1);
+	}
+
+    private void construct(String[] arr,int ind,Queue<Node> queue)
+    {
+        if(ind>=arr.length)
+        return;
+        if(queue.size()==0)
+        {
+            Node nn=new Node(Integer.parseInt(arr[ind]));
+            this.root=nn;
+            queue.add(nn);
+        }
+        else
+        {
+            Node parent=queue.peek();
+                if(parent.data!=-1){
+                if(parent.left==null)
+                {
+                    parent.left=new Node(Integer.parseInt(arr[ind]));
+                    queue.add(parent.left);
+                }
+                else
+                {
+                    if(parent.right==null)
+                    {
+                    parent.right=new Node(Integer.parseInt(arr[ind]));
+                    queue.add(parent.right);
+                    queue.poll();
+                    }
+               }
+               }
+               else
+               {
+                   queue.poll();
+                   ind--;
+               }
+        }
+        construct(arr,ind+1,queue);
+    }
+
+    public void display()
+    {
+        display_tree(this.root);
+    }
+
+    private void display_tree(Node root)
+    {
+        if(root==null)
+        return;
+        String str=root.data+"";
+        if(root.left!=null)
+        {
+            str=root.left.data+" <= "+str;
+        }
+        else
+        {
+            str="END <= "+str;
+        }
+
+        if(root.right!=null)
+        {
+            str=str+" => "+root.right.data;
+        }
+        else
+        {
+            str=str+" => END";
+        }
+        System.out.println(str);
+        display_tree(root.left);
+        display_tree(root.right);
+
+    }
+
+
+}
